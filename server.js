a2b870ed9c541be591c5243dd803d284b494c906
Andrew Henderson <ashenderson4372@gmail.com>
2022-06-03 18:22:44 -0400

    added dl

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express")
 const res = require("express/lib/response")
+const cors =  require('cors')
 const app = express()
 const PORT = 8000
 
-const rappers = {
+app.use(cors())
+
+/*const rappers = {
     "21 savage": {
         'age': 29,
         "birthName": 'Shéyaa Bin Abraham-Joseph',
@@ -19,22 +22,98 @@ const rappers = {
         'birthName': 'unknown',
         'birthLocation': 'unknown'
     }
-}
+}*/
 
 
 app.get('/', (request, response) => {
     response.sendFile(__dirname + '/index.html')                                                //looks in the main directory for index.html
 })
 
-app.get('/api/:name', (request, response) => {                                  
-    const name = request.params.name.toLowerCase()                                              //pulls the parameter out of the url and assigns it to a variable.  REQUEST.PARAMS.${NAME}                            
-    if( rappers[name] ){
-        response.json(rappers[name])                                                  //Put what you want to respond with inside the .json parentheses uses brackets because .notation doesnt work with spaces. Takes the query param and passes it as a key in the rappers object 
+app.get('/api/:commander', (request, response) => {
+    response.sendFile(__dirname + '/decklist.html')                                 
+    const name = request.params.commander.toLowerCase()                                              //pulls the parameter out of the url and assigns it to a variable.  REQUEST.PARAMS.${NAME}                            
+    if( deck[name] ){
+        response.json(deck[name])                                                                           //Put what you want to respond with inside the .json parentheses uses brackets because .notation doesnt work with spaces. Takes the query param and passes it as a key in the rappers object 
     } else {
         response.json(rappers['unknown'])
     }
 })
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {                                                //environment variable OR hardcoded port
     console.log(`Listning on port ${PORT}`)
 })
+
+
+const deck = {
+    "elspeth": {
+        "Commander": 'Elspeth Resplendent',
+        "Cards": [
+            'The Wandering Emperor', 
+            'Plains', 
+            'Gideon Blackblade', 
+            'Castle Ardenvale', 
+            "Elspeth, Sun's Nemesis", 
+            "Emeria's Call", 
+            'Teyo, Aegis Adept', 
+            'Ugin, the Ineffable' ,
+            'Eiganjo, Seat of the Empire',
+            'Esper Sentinel' ,
+            'Hopeful Initiate', 
+            'Swords to Plowshares' ,
+            'Luminarch Aspirant' ,
+            'Ambitious Farmhand' ,
+            'Unlicensed Hearse',
+            'Pacifism' ,
+            'Rune of Sustenance' ,
+            'Spirited Companion',
+            'Seal Away' ,
+            'Thalia, Guardian of Thraben' ,
+        ' Tocatli Honor Guard' ,
+            'Tithe Taker',
+            'Valorous Stance ',
+            'Adeline, Resplendent Cathar',
+            'Banishing Light' ,
+            'Borrowed Time' ,
+            'Elite Spellbinder', 
+            'Inspiring Overseer', 
+            'Priest of Ancient Lore' ,
+            'Skyclave Apparition ',
+            'Prison Realm ',
+            'The Restoration of Eiganjo' ,
+            'Vryn Wingmare' ,
+            'Archon of Absolution' ,
+            'Felidar Retreat' ,
+            'Baird, Steward of Argive' ,
+            'Elesh Norn, Grand Cenobite', 
+            'Rumor Gatherer' ,
+            'Cosmos Elixir' ,
+            'Lion Sash' ,
+            'Welcoming Vampire' ,
+            'Cathar Commando' ,
+        ' Hushbringer' ,
+            'Linvala, Keeper of Silence' ,
+        ' Smothering Tithe',
+            "Oketra's Monument", 
+            'Mangara, the Diplomat' ,
+            "Kinjalli's Sunwing" ,
+            'Solemn Simulacrum' ,
+            "Heliod's Intervention" ,
+            'March of Otherworldly Light' ,
+            'Settle the Wreckage' ,
+            'Farewell' ,
+            'Wrath of God' ,
+            'Mind Stone' ,
+            'Hedron Archive' ,
+            'Arcane Signet' ,
+            'Coldsteel Heart', 
+            'Guardian Idol' ,
+            'Archon of Emeria' ,
+            'Reidane, God of the Worthy' ,
+            'Arch of Orazca' ,
+            'Field of Ruin' ,
+            'Elspeth Conquers Death' ,
+            'Loyal Warhound' ,
+            'Celestial Vault' ,
+            ],
+        }
+}
