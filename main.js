90ec124a62036075d036bf498fe3bcfa0ea083d5
Andrew Henderson <ashenderson4372@gmail.com>
2022-06-03 20:00:03 -0400

    changed url

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 async function apiRequest(){
     const commanderName = document.querySelector('input').value
     try {
-        const response = await fetch(URL)
+        const response = await fetch(`https://simple-commander-api.herokuapp.com/api/${commanderName}`)
         const data = await response.json()
 
         document.querySelector('h1').innerText = data['commander']
